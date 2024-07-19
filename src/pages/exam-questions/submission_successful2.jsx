0f905c49d9ca12f69ab5356bff3f2de626a7be47
Rutuja5871 <rutujakatkar121@gmail.com>
2024-07-19 12:22:59 +0530

    hello

--- src/pages/exam-questions/submission_successful2.jsx
+++ src/pages/exam-questions/submission_successful2.jsx
@@ -4,9 +4,8 @@ import Sidebar from "../../partials/Sidebar";
 import Header from "../../partials/Header";
 import { useLocation, useNavigate } from "react-router-dom";
 import { baseurl } from "../../utils/domain";
-import successGif1 from "../../images/animation-2.gif"; // Import the GIF
-import successGif2 from "../../images/animation-3.gif";
-import "./exam-questions.css"; // Import the CSS file for styling
+import successGif from '../../images/suucess-animation-1.gif'; // Import the GIF
+import './exam-questions.css'; // Import the CSS file for styling
 
 function submission_success() {
   const location = useLocation();
@@ -47,41 +46,16 @@ function submission_success() {
     fetchUserData();
   }, [seid]);
 
+
   return (
-    <div
-      className="d-flex justify-content-center align-items-center vh-100"
-      style={{ background: "#7ca0a9" }}
-    >
+    <div className="flex h-screen overflow-hidden">
       <div className="success-page">
-        <div>
-          <p>Dear,</p>
-          <name1>
-            <strong>{userData.first_name + " " + userData.last_name}</strong>
-          </name1>
-          <p>Thanks for Submitting,</p>
-          <h2>{userData.exam_name}</h2>
-        </div>
-        <div>
-          {userData.gender === "female" && (
-            <img
-              src={successGif1}
-              alt="Success Animation"
-              className="success-gif"
-            />
-          )}
-          {userData.gender === "male" && (
-            <img
-              src={successGif2}
-              alt="Success Animation"
-              className="success-gif"
-            />
-          )}
-        </div>
-        <div>
-          <p1>Your Exam is Submitted Successfully!</p1>
-          <hr />
-          <p>You will receive the Result and Certificate on Email.</p>
-        </div>
+        <h1>Dear, <strong>{userData.first_name+" "+ userData.last_name}</strong></h1>
+        <p>Thanks for Submitting, {userData.exam_name}</p>
+        <img src={successGif} alt="Success Animation" className="success-gif" />
+        <p className="m-4">Your Exam is Submitted Successfully!</p>
+        <hr />
+        <p className="m-4">You  will recieve Result and Certificate on Email.</p>
       </div>
     </div>
   );
